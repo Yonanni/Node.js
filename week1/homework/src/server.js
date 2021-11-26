7d49a80af042123ffb48d91e7a0d87311c34b32e
Yonanni <adhanamengisteab@gmail.com>
2021-11-26 22:33:36 +0100

    //

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -9,7 +9,7 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    
   });
 
   return server;
